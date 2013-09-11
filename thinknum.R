567bb1f7aa2130c5364c7f6981b56428fae33a4d
vinayakh <vinayakh@gmail.com>
2013-09-12 01:16:58 +0530

    Plotting public datasets using thinknum

---
+++ thinknum.R
@@ -0,0 +1,67 @@
+# Description : Plotting public datsets using Thinknum
+# Website : http://www.econometricsbysimulation.com/2013/09/thinknum-new-interactive-public.html
+
+doInstall <- TRUE # Change to FALSE if you don't want packages installed.
+toInstall <- c("Thinknum")
+if(doInstall){install.packages(toInstall, repos = "http://cran.r-project.org")}
+lapply(toInstall, library, character.only = TRUE)
+
+library("Thinknum")
+akam <- Thinknum("akam")
+plot(akam, type="l")
+
+
+
+# In order to pass arguments to R in the same manner as the ThinkNum website I have written a small function which can read multiple calls to Thinknum and returns them as either a wide or tall data.frame
+
+mThinknum <- function(command, tall=F) {
+  
+  require("Thinknum")
+  
+  # Break the command into seperate calls
+  think.list <- strsplit(command, ";")[[1]]
+  
+  # Look through each element of the think.list
+  for (i in 1:length(think.list)) {
+    
+    cat(paste("Reading:",think.list[i])) # Display feedback
+    
+    if (!tall) {
+      if (i==1) returner <- Thinknum(think.list[1])
+      if (i>1)  returner <- merge(returner,Thinknum(think.list[i])
+                                  , by.x="date_time", by.y="date_time")
+    }
+    if (tall) {
+      tempdat <- Thinknum(think.list[i])
+      names(tempdat) <- c("date_time", "value")
+      
+      if (i==1) returner <- data.frame(tempdat, call=think.list[i])
+      if (i>1) returner <- rbind(returner, data.frame(tempdat, call=think.list[i]))
+      names(returner) <- c("date_time", "value", "call")
+    }
+    
+    cat(paste(rep(" ", max(1,20-nchar(think.list[i]))), collapse=""))
+    # Insert spaces
+    cat(paste("Dimensions:", paste(dim(returner), collapse="x"), "\n"))
+    # Show dimensions
+  }
+  
+  # Ensure the return file has appropriate column names
+  if (!tall) names(returner) <- c("date_time", think.list)
+  
+  data.frame(returner)
+}
+
+test <- mThinknum("^spx;sma(^spx,30);sma(goog*2,30);(goog*2)")
+head(test)
+
+# Let's try plotting with the base package
+
+plot(x=c(min(test$date_time), max(test$date_time)),
+     y=c(min(test[,2:5]),max(test[,2:5])), type="n",
+     main="Plot in R")
+
+lines(test$date_time, test[,2], type="l")
+lines(test$date_time, test[,3], type="l", col="red")
+lines(test$date_time, test[,4], type="l", col="blue")
+lines(test$date_time, test[,5], type="l", col="darkgreen")
