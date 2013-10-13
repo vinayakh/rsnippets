6e6975396780fd46a2ec3e0bec9d2963417553c6
vinayakh <vinayakh@gmail.com>
2013-10-13 14:28:24 +0530

    Kernel Smoothing Part 2

---
+++ kernel-smoothing-part2.R
@@ -0,0 +1,88 @@
+# Description : Kernel Smoothing part 2
+# Website : http://freakonometrics.hypotheses.org/9184
+
+# Generate Random data
+set.seed(1)
+n=10
+xr = seq(0,n,by=.1)
+yr = sin(xr/2)+rnorm(length(xr))/2
+db = data.frame(x=xr,y=yr)
+plot(db)
+
+
+attach(db)
+library(splines)
+B=bs(xr,knots=c(3),Boundary.knots=c(0,10),degre=1)
+reg=lm(yr~B)
+lines(xr[xr<=3],predict(reg)[xr<=3],col="red")
+lines(xr[xr>=3],predict(reg)[xr>=3],col="blue")
+
+reg=lm(yr~xr,subset=xr<=3)
+lines(xr[xr<=3],predict(reg)[xr<=3],col="red",lty=2)
+reg=lm(yr~xr,subset=xr>=3)
+lines(xr[xr>=3],predict(reg),col="blue",lty=2)
+
+reg=lm(yr~bs(xr,knots=c(3),Boundary.knots=c(0,10),degre=1),data=db)
+
+B=bs(xr,knots=c(2,5),Boundary.knots=c(0,10),degre=1)
+matplot(xr,B,type="l")
+abline(v=c(0,2,5,10),lty=2)
+
+B=bs(xr,knots=c(2,5),Boundary.knots=c(0,10),degre=1)
+matplot(xr,B,type="l")
+abline(v=c(0,2,5,10),lty=2)
+
+reg=lm(yr~B)
+lines(xr,predict(reg),col="red")
+
+B=bs(xr,knots=1:9,Boundary.knots=c(0,10),degre=1)
+reg=lm(yr~B)
+lines(xr,predict(reg),col="red")
+
+reg=lm(yr~B)
+P=predict(reg,interval="confidence")
+plot(db,col="white")
+polygon(c(xr,rev(xr)),c(P[,2],rev(P[,3])),col="light blue",border=NA)
+points(db)
+reg=lm(yr~B)
+lines(xr,P[,1],col="red")
+abline(v=c(0,2,5,10),lty=2)
+
+B=bs(xr,knots=c(2,5),Boundary.knots=c(0,10),degre=2)
+matplot(xr,B,type="l")
+abline(v=c(0,2,5,10),lty=2)
+
+k=2
+plot(db)
+B=cbind(1,B)
+lines(xr,B[,1:k]%*%coefficients(reg)[1:k],col=k-1,lty=k-1)
+
+k=3
+plot(db)
+B=cbind(1,B)
+lines(xr,B[,1:k]%*%coefficients(reg)[1:k],col=k-1,lty=k-1)
+
+k=4
+plot(db)
+B=cbind(1,B)
+lines(xr,B[,1:k]%*%coefficients(reg)[1:k],col=k-1,lty=k-1)
+
+k=5
+plot(db)
+B=cbind(1,B)
+lines(xr,B[,1:k]%*%coefficients(reg)[1:k],col=k-1,lty=k-1)
+
+reg=lm(yr~B)
+P=predict(reg,interval="confidence")
+plot(db,col="white")
+polygon(c(xr,rev(xr)),c(P[,2],rev(P[,3])),col="light blue",border=NA)
+points(db)
+reg=lm(yr~B)
+lines(xr,P[,1],col="red")
+abline(v=c(0,2,5,10),lty=2)
+
+plot(data$no,data$mu,ylim=c(6,10))
+abline(v=12*(0:8)+.5,lty=2)
+reg=lm(mu~bs(no,knots=c(12*(1:7)+.5),Boundary.knots=c(0,97),
+             degre=1),data=db)
+lines(c(1:94,96),predict(reg),col="red")
